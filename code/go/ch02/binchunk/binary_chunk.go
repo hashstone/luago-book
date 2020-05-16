7434eaa5a0c8420225bf202e150063ab5ef40432
fanjunliang <fanjunliang@sensetime.com>
2020-05-16 20:29:32 +0800

    add ch02 code

---
+++ code/go/ch02/binchunk/binary_chunk.go
@@ -0,0 +1,180 @@
+package binchunk
+
+const (
+	LUA_SIGNATURE    = "\x1bLua"
+	LUAC_VERSION     = 0x53
+	LUAC_FORMAT      = 0
+	LUAC_DATA        = "\x19\x93\r\n\x1a\n"
+	CINT_SIZE        = 4
+	CSIZET_SIZE      = 8
+	INSTRUCTION_SIZE = 4
+	LUA_INTEGER_SIZE = 8
+	LUA_NUMBER_SIZE  = 8
+	LUAC_INT         = 0x5678
+	LUAC_NUM         = 370.5
+)
+
+const (
+	TAG_NIL       = 0x00
+	TAG_BOOLEAN   = 0x01
+	TAG_NUMBER    = 0x03
+	TAG_INTEGER   = 0x13
+	TAG_SHORT_STR = 0x04
+	TAG_LONG_STR  = 0x14
+)
+
+type binaryChunk struct {
+	header
+	sizeUpvalues byte
+	mainFunc     *Prototype
+}
+
+type header struct {
+	signatrure      [4]byte
+	version         byte
+	format          byte
+	luacData        [6]byte
+	cintSize        byte
+	sizetSize       byte
+	instructionSize byte
+	luaIntegerSize  byte
+	luaNumberSize   byte
+	luacInt         int64
+	luacNum         float64
+}
+
+type Prototype struct {
+	Source          string
+	LineDefined     uint32
+	LastLineDefined uint32
+	NumParams       byte
+	IsVararg        byte
+	MaxStackSize    byte
+	Code            []uint32
+	Constants       []interface{}
+	Upvalues        []Upvalue
+	Protos          []*Prototype
+	LineInfo        []uint32
+	LocVars         []LocVar
+	UpvalueNames    []string
+}
+
+type Upvalue struct {
+	Instack byte
+	Idx     byte
+}
+
+type LocVar struct {
+	VarName string
+	StartPC uint32
+	EndPC   uint32
+}
+
+func Undump(data []byte) *Prototype {
+	reader := &reader{data}
+	reader.checkHeader()
+	reader.readByte()
+	return reader.readProto("")
+}
+
+func (self *reader) readProto(parentSource string) *Prototype {
+	source := self.readString()
+	if source == "" {
+		source = parentSource
+	}
+	return &Prototype{
+		Source:          source,
+		LineDefined:     self.readUint32(),
+		LastLineDefined: self.readUint32(),
+		NumParams:       self.readByte(),
+		IsVararg:        self.readByte(),
+		MaxStackSize:    self.readByte(),
+		Code:            self.readCode(),
+		Constants:       self.readConstants(),
+		Upvalues:        self.readUpvalues(),
+		Protos:          self.readProtos(source),
+		LineInfo:        self.readLineInfo(),
+		LocVars:         self.readLocVars(),
+		UpvalueNames:    self.readUpvalueNames(),
+	}
+}
+
+func (self *reader) readCode() []uint32 {
+	code := make([]uint32, self.readUint32())
+	for i := range code {
+		code[i] = self.readUint32()
+	}
+	return code
+}
+
+func (self *reader) readConstants() []interface{} {
+	constants := make([]interface{}, self.readUint32())
+	for i := range constants {
+		constants[i] = self.readConstant()
+	}
+	return constants
+}
+
+func (self *reader) readConstant() interface{} {
+	switch self.readByte() {
+	case TAG_NIL:
+		return nil
+	case TAG_BOOLEAN:
+		return self.readByte() != 0
+	case TAG_INTEGER:
+		return self.readLuaInteger()
+	case TAG_NUMBER:
+		return self.readLuaNumber()
+	case TAG_SHORT_STR, TAG_LONG_STR:
+		return self.readString()
+	default:
+		panic("corrupted!") // todo
+	}
+}
+
+func (self *reader) readUpvalues() []Upvalue {
+	upvalues := make([]Upvalue, self.readUint32())
+	for i := range upvalues {
+		upvalues[i] = Upvalue{
+			Instack: self.readByte(),
+			Idx:     self.readByte(),
+		}
+	}
+	return upvalues
+}
+
+func (self *reader) readProtos(parentSource string) []*Prototype {
+	protos := make([]*Prototype, self.readUint32())
+	for i := range protos {
+		protos[i] = self.readProto(parentSource)
+	}
+	return protos
+}
+
+func (self *reader) readLineInfo() []uint32 {
+	lineInfo := make([]uint32, self.readUint32())
+	for i := range lineInfo {
+		lineInfo[i] = self.readUint32()
+	}
+	return lineInfo
+}
+
+func (self *reader) readLocVars() []LocVar {
+	locVars := make([]LocVar, self.readUint32())
+	for i := range locVars {
+		locVars[i] = LocVar{
+			VarName: self.readString(),
+			StartPC: self.readUint32(),
+			EndPC:   self.readUint32(),
+		}
+	}
+	return locVars
+}
+
+func (self *reader) readUpvalueNames() []string {
+	names := make([]string, self.readUint32())
+	for i := range names {
+		names[i] = self.readString()
+	}
+	return names
+}
---
+++ code/go/ch02/binchunk/reader.go
@@ -0,0 +1,90 @@
+package binchunk
+
+import (
+	"encoding/binary"
+	"math"
+)
+
+type reader struct {
+	data []byte
+}
+
+func (self *reader) readByte() byte {
+	b := self.data[0]
+	self.data = self.data[1:]
+	return b
+}
+
+func (self *reader) readUint32() uint32 {
+	i := binary.LittleEndian.Uint32(self.data)
+	self.data = self.data[4:]
+	return i
+}
+
+func (self *reader) readUint64() uint64 {
+	i := binary.LittleEndian.Uint64(self.data)
+	self.data = self.data[8:]
+	return i
+}
+
+func (self *reader) readLuaInteger() int64 {
+	return int64(self.readUint64())
+}
+
+func (self *reader) readLuaNumber() float64 {
+	return math.Float64frombits(self.readUint64())
+}
+
+func (self *reader) readString() string {
+	size := uint(self.readByte())
+	if size == 0 {
+		return ""
+	}
+	if size == 0xFF {
+		size = uint(self.readUint64())
+	}
+	bytes := self.readBytes(size - 1)
+	return string(bytes)
+}
+
+func (self *reader) readBytes(n uint) []byte {
+	bytes := self.data[:n]
+	self.data = self.data[n:]
+	return bytes
+}
+
+func (self *reader) checkHeader() {
+	if string(self.readBytes(4)) != LUA_SIGNATURE {
+		panic("not a precompiled chunk!")
+	}
+	if self.readByte() != LUAC_VERSION {
+		panic("version mismatch!")
+	}
+	if self.readByte() != LUAC_FORMAT {
+		panic("format mismatch!")
+	}
+	if string(self.readBytes(6)) != LUAC_DATA {
+		panic("corrupted!")
+	}
+	if self.readByte() != CINT_SIZE {
+		panic("int size mismatch!")
+	}
+	if self.readByte() != CSIZET_SIZE {
+		panic("size_t size mismatch!")
+	}
+	if self.readByte() != INSTRUCTION_SIZE {
+		panic("instruction size mismatch!")
+	}
+	if self.readByte() != LUA_INTEGER_SIZE {
+		panic("lua_Integer size mismatch!")
+	}
+	if self.readByte() != LUA_NUMBER_SIZE {
+		panic("lua_Number size mismatch!")
+	}
+	if self.readLuaInteger() != LUAC_INT {
+		panic("endianness mismatch!")
+	}
+	if self.readLuaNumber() != LUAC_NUM {
+		panic("float format mismatch!")
+	}
+}
